966d816887370bd577c880c13d0450d979260b0f
Joshuavarghese527 <joshuavarghese527@gmail.com>
2018-05-23 19:57:33 -0400

    Band Scales in D3

--- svgd3/js/main.js
+++ svgd3/js/main.js
@@ -16,18 +16,29 @@ d3.json("data/buildings.json").then(function(data){
         d.height = +d.height;
     });
 
+    var x = d3.scaleBand()
+        .domain(["Burj Khalifa", "Shanghai Tower",
+                 "Abraj Al-Bait Clock Tower", "Ping An Finance Centre",
+                 "Lotte World Tower"])
+        .range([0, 400])
+        .paddingInner(0.3)
+        .paddingOuter(0.3);
+
+        console.log(x("Burj Khalifa"));
+
     var y = d3.scaleLinear()
         .domain([0, 828])
         .range([0, 400]);
 
     var rects = svg.selectAll("rect")
             .data(data)
-        .enter().append("rect")
-            .attr("y", 0)
-            .attr("x", function(d, i){
-                return (i * 60);
+            .enter()
+            .append("rect")
+            .attr("y", 20)
+            .attr("x", function(d){
+                return x(d.name);
             })
-            .attr("width", 40)
+            .attr("width", x.bandwidth)
             .attr("height", function(d){
                 return y(d.height);
             })
